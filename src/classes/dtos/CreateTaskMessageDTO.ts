9c3988a475d2fbce8373ca72474b9faabdce6221
diogorodriguees <diogorodrigueslife@gmail.com>
2024-02-16 23:32:59 -0300

    :recycle: change console per logger

--- src/classes/dtos/CreateTaskMessageDTO.ts
+++ src/classes/dtos/CreateTaskMessageDTO.ts
@@ -1,3 +1,3 @@
 export class CreateTaskMessageDTO {
-  value: string;
+  content: string;
 }
--- src/consumers/TaskConsumer.ts
+++ src/consumers/TaskConsumer.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { CreateTaskMessageDTO } from 'src/classes/dtos/CreateTaskMessageDTO';
 
@@ -6,6 +6,6 @@ import { CreateTaskMessageDTO } from 'src/classes/dtos/CreateTaskMessageDTO';
 export class TaskConsumer {
   @MessagePattern('CREATE_TASK')
   async consumeCreateTask(message: CreateTaskMessageDTO) {
-    console.log(message.value);
+    Logger.log(message.content, 'Message Consumer');
   }
 }
